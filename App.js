995066f209d9783d0a4cd44e5bbb861dbebe5bb0
hrecker <henryjrecker@gmail.com>
2018-06-02 22:11:34 -0500

    Made back button work when moving between screens

--- App.js
+++ App.js
@@ -1,31 +1,51 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createDrawerNavigator } from 'react-navigation';
+import { createDrawerNavigator, createStackNavigator } from 'react-navigation';
 import HomeScreen from './HomeScreen.js';
 import CreateRecipeScreen from './CreateRecipeScreen.js';
 import RecipeListScreen from './RecipeListScreen.js';
 import ViewRecipeScreen from './ViewRecipeScreen.js';
 
-const RootStack = createDrawerNavigator({
+const InnerStack = createStackNavigator({
   Home: HomeScreen,
   CreateRecipe: CreateRecipeScreen,
   RecipeList: RecipeListScreen,
-  ViewRecipe: ViewRecipeScreen,
+  ViewRecipe: ViewRecipeScreen
 }, {
-    initialRouteName: 'Home',
+  initialRouteName: 'Home',
+  headerMode: 'none',
+  navigationOptions: {
+    headerVisible: false
+  }
+})
+
+class Hidden extends React.Component {
+  render() {
+    return null;
+  }
+}
+
+const RootDrawer = createDrawerNavigator({
+  Stack: {
+    screen: InnerStack,
     navigationOptions: {
-      headerStyle: {
-        backgroundColor: '#f4511e',
-      },
-      headerTintColor: '#fff',
-      headerTitleStyle: {
-        fontWeight: 'bold',
-      },
+      drawerLabel: <Hidden />
     }
-  });
+  },
+  Home: HomeScreen,
+  CreateRecipe: CreateRecipeScreen,
+  RecipeList: RecipeListScreen,
+  ViewRecipe: ViewRecipeScreen
+}, {
+    initialRouteName: 'Stack',
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false
+    }
+});
 
 export default class App extends Component {
   render() {
-    return (<RootStack />);
+    return (<RootDrawer />);
   }
 }
